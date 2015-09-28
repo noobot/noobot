fb4d1bf6b4394d12448833d21fff43f61dc57722
Simon Colmer <simon_colmer@hotmail.com>
2015-09-28 19:31:40 +0100

    Config reading tests

---
+++ src/Noobot.Tests.Unit/Domain/Configuration/ConfigReadingTests.cs
@@ -0,0 +1,24 @@
+﻿using Noobot.Domain.Configuration;
+using NUnit.Framework;
+using Should;
+
+namespace Noobot.Tests.Unit.Domain.Configuration
+{
+    [TestFixture]
+    public class ConfigReadingTests
+    {
+        [Test]
+        public void should_read_config()
+        {
+            // given
+
+            // when
+            var config = Config.GetConfig();
+
+            // then
+            config.ShouldNotBeNull();
+            config.Slack.ShouldNotBeNull();
+            config.Slack.ApiToken.ShouldNotBeNull();
+        }
+    }
+}
