67cb6faa8e0da786ef48a087396a1cbe53da0bc8
Simon Colmer <simon_colmer@hotmail.com>
2015-09-29 22:13:40 +0100

    Can now reply to messages

--- src/Noobot.Tests.Unit/Domain/Slack/ListenerTests.cs
+++ src/Noobot.Tests.Unit/Domain/Slack/ListenerTests.cs
@@ -17,22 +17,37 @@ namespace Noobot.Tests.Unit.Domain.Slack
             // given
             Config config = new ConfigReader().GetConfig();
             var client = new SlackSocketClient(config.Slack.ApiToken);
-            
+            LoginResponse loginResponsey;
+
             // when
             client.Connect(loginResponse =>
             {
                 //This is called once the client has emitted the RTM start command
-                var bots = loginResponse.bots;
-                var user = loginResponse.users.FirstOrDefault(x => x.id.Equals("U025U3V33", StringComparison.InvariantCultureIgnoreCase));
-            }, () => {
-                //This is called once the RTM client has connected to the end point
-               config = config;
-            });
+                loginResponsey = loginResponse;
 
-            client.OnMessageReceived += delegate(NewMessage message)
+            }, () =>
             {
-                Console.WriteLine(message.text);
-            };
+                //This is called once the Real Time Messaging client has connected to the end point
+
+                client.OnMessageReceived += delegate (NewMessage message)
+                {
+                    Console.WriteLine(message.text);
+                    var response = new NewMessage
+                    {
+                        channel = message.channel,
+                        user = message.user,
+                        text = "Hi - TEST"
+                    };
+
+                    //client.Message(response);
+                    client.SendMessage(messageReceived =>
+                    {
+                        response.channel = messageReceived.text;
+
+                    }, message.channel, "HI - MEGA TEST");
+                };
+
+            });
 
             // then
             for (int i = 0; i < 10000; i++)
