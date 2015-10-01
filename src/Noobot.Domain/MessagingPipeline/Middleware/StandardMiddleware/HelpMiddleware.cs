06d90652a3dbfca0cade4cdb37cba1fb8c311d6f
Simon Colmer <simon_colmer@hotmail.com>
2015-10-01 15:46:57 +0100

    Working on migrating message handling over

--- src/Noobot.Domain/MessagingPipeline/Middleware/StandardMiddleware/HelpMiddleware.cs
+++ src/Noobot.Domain/MessagingPipeline/Middleware/StandardMiddleware/HelpMiddleware.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NDesk.Options;
 using Noobot.Domain.MessagingPipeline.Request;
 using Noobot.Domain.MessagingPipeline.Response;
 
@@ -8,11 +9,50 @@ namespace Noobot.Domain.MessagingPipeline.Middleware.StandardMiddleware
 {
     public class HelpMiddleware : MiddlewareBase
     {
+        private HandlerMapping[] _handlerMappings;
+
         public HelpMiddleware(IMiddleware next) : base(next)
-        { }
+        {
+            _handlerMappings = new[]
+            {
+                new HandlerMapping
+                {
+                    ValidHandles = new [] {"help", "yo tell me"},
+                    Description = "Returns supported commands and descriptions of how to use them",
+                    EvaluatorFunc = HelpHandler
+                }
+            };
+        }
+
+        private IEnumerable<ResponseMessage> HelpHandler(IncomingMessage message)
+        {
+            return new ResponseMessage[0];
+        }
 
         public override IEnumerable<ResponseMessage> Invoke(IncomingMessage message)
         {
+
+
+            var p = new OptionSet
+            {
+                {"h|help", s =>
+                    {
+                        var builder = new StringBuilder();
+                        builder.Append(">>>");
+
+                        foreach (var commandDescription in GetSupportedCommands())
+                        {
+                            builder.AppendFormat("{0}\t- {1}\n", commandDescription.Command, commandDescription.Description);
+                        }
+
+                        yield return message.ReplyDirectlyToUser(builder.ToString());
+                    }
+                }
+            };
+
+
+
+
             if (message.Text.Equals("help", StringComparison.InvariantCultureIgnoreCase))
             {
                 var builder = new StringBuilder();
@@ -36,7 +76,7 @@ namespace Noobot.Domain.MessagingPipeline.Middleware.StandardMiddleware
 
         protected override CommandDescription[] SupportedCommands()
         {
-            return new []
+            return new[]
             {
                 new CommandDescription
                 {
