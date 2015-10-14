f55bf5adbc58528b0f701397d3772506e212e8c2
Simon Colmer <simon_colmer@hotmail.com>
2015-10-14 19:00:59 +0100

    Added example of middleware that responds to ANY message

---
+++ src/Noobot.Custom/Pipeline/Middleware/AutoResponderMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Noobot.Domain.MessagingPipeline.Middleware;
+using Noobot.Domain.MessagingPipeline.Request;
+using Noobot.Domain.MessagingPipeline.Response;
+
+namespace Noobot.Custom.Pipeline.Middleware
+{
+    public class AutoResponderMiddleware : MiddlewareBase
+    {
+        public AutoResponderMiddleware(IMiddleware next) : base(next)
+        {
+            HandlerMappings = new []
+            {
+                new HandlerMapping
+                {
+                    ValidHandles = new [] { ""},
+                    Description = "Annoys the heck out of everyone",
+                    EvaluatorFunc = AutoResponseHandler,
+                    FilterMessagesDirectedAtBot = false,
+                    ShouldContinueProcessing = true
+                }
+            };
+        }
+
+        private IEnumerable<ResponseMessage> AutoResponseHandler(IncomingMessage message, string matchedHandle)
+        {
+            yield return message.ReplyDirectlyToUser(message.Text);
+        }
+    }
+}
