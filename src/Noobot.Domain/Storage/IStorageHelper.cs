7f98373ecc2fdf91ea713a600d5785609cdc990c
Simon C <simon_colmer@hotmail.com>
2015-11-16 14:34:23 +0000

    Added basic StorageHelper class

---
+++ src/Noobot.Domain/Storage/IStorageHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace Noobot.Domain.Storage
+{
+    public interface IStorageHelper
+    {
+        T[] ReadFile<T>(string fileName) where T : class;
+        void SaveFile<T>(string fileName, T[] objects) where T : class;
+        void DeleteFile(string fileName);
+    }
+}
---
+++ src/Noobot.Domain/Storage/StorageHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace Noobot.Domain.Storage
+{
+    public class StorageHelper : IStorageHelper
+    {
+        public T[] ReadFile<T>(string fileName) where T : class
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SaveFile<T>(string fileName, T[] objects) where T : class
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteFile(string fileName)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
