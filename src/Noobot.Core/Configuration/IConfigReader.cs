ed14011eb41ea058ed36b4c3141b7a6edb6ea71a
Simon C <simon_colmer@hotmail.com>
2016-01-11 08:12:47 +0000

    Removed unused nuget packages

--- src/Noobot.Core/Configuration/IConfigReader.cs
+++ src/Noobot.Core/Configuration/IConfigReader.cs
@@ -2,8 +2,19 @@
 {
     public interface IConfigReader
     {
+        /// <summary>
+        /// Should return the API key required to connect to your team.
+        /// </summary>
         string SlackApiKey();
+
+        /// <summary>
+        /// Should the "help" middleware be used?
+        /// </summary>
         bool HelpEnabled();
+
+        /// <summary>
+        /// Should return any other configuration values you need within your middleware/plugins.
+        /// </summary>
         T GetConfigEntry<T>(string entryName);
     }
 }
--- src/Noobot.Core/NoobotCore.cs
+++ src/Noobot.Core/NoobotCore.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
 using Noobot.Core.Configuration;
 using Noobot.Core.DependencyResolution;
 using Noobot.Core.Logging;
