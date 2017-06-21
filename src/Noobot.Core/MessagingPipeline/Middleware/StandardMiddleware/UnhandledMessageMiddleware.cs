93320ae1b08b8df26ce32d98ce378cf9d5b0a788
PHeonix25 <p@hermens.com.au>
2017-06-21 17:51:47 +0200

    Makes the UnhandledMessage friendlier

--- src/Noobot.Core/MessagingPipeline/Middleware/StandardMiddleware/UnhandledMessageMiddleware.cs
+++ src/Noobot.Core/MessagingPipeline/Middleware/StandardMiddleware/UnhandledMessageMiddleware.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
 using Common.Logging;
 using Noobot.Core.MessagingPipeline.Request;
 using Noobot.Core.MessagingPipeline.Response;
@@ -19,13 +24,15 @@ namespace Noobot.Core.MessagingPipeline.Middleware.StandardMiddleware
 
         public IEnumerable<ResponseMessage> Invoke(IncomingMessage message)
         {
-            _log.Info("Unhandled");
-            return new ResponseMessage[0];
+            _log.Info("Unhandled message.");
+
+            if(message.ChannelType == ResponseType.DirectMessage)
+            {
+                yield return message.ReplyToChannel("Sorry, I didn't understand that request.");
+                yield return message.ReplyToChannel("Just type `help` so I can show you what I can do!");
+            }
         }
 
-        public IEnumerable<CommandDescription> GetSupportedCommands()
-        {
-            return new CommandDescription[0];
-        }
+        public IEnumerable<CommandDescription> GetSupportedCommands() => new CommandDescription[0];
     }
 }
