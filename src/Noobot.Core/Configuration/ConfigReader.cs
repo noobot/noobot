b1dc7bbda59c05193ef1e5dcfad44f69dcfb0cb0
Ashley Poole <git@ashleypoole.co.uk>
2017-09-12 12:47:38 +0100

    Allow config.json path to work on non-Windows OS
    Changed the config.json path so it works on non-Windows OS where
    forward slashes are used instead of backslashes.

--- src/Noobot.Core/Configuration/ConfigReader.cs
+++ src/Noobot.Core/Configuration/ConfigReader.cs
@@ -10,7 +10,7 @@ namespace Noobot.Core.Configuration
         private JObject _currentJObject;
         private readonly string _configLocation;
         private readonly object _lock = new object();
-        private const string DEFAULT_LOCATION = @"configuration\config.json";
+        private static readonly string DEFAULT_LOCATION = Path.Combine("configuration", "config.json");
         private const string SLACKAPI_CONFIGVALUE = "slack:apiToken";
 
         public ConfigReader() : this(DEFAULT_LOCATION) { }
