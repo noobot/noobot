db2bd227f520cd5fedf602dfe5b9e15c8f0d9c9c
Jakub A. Zimny <jakub.zimny@pl.ibm.com>
2017-11-15 12:14:10 +0100

    Add lookup by user id

--- src/Noobot.Core/NoobotCore.cs
+++ src/Noobot.Core/NoobotCore.cs
@@ -252,6 +252,12 @@ namespace Noobot.Core
             return string.IsNullOrEmpty(user.Key) ? string.Empty : user.Key;
         }
 
+        public string GetUserIdForUserEmail(string email)
+        {
+            var user = _connection.UserCache.FirstOrDefault(x => x.Value.Email.Equals(email, StringComparison.OrdinalIgnoreCase));
+            return string.IsNullOrEmpty(user.Key) ? string.Empty : user.Key;
+        }
+
         public string GetChannelId(string channelName)
         {
             var channel = _connection.ConnectedChannels().FirstOrDefault(x => x.Name.Equals(channelName, StringComparison.OrdinalIgnoreCase));
