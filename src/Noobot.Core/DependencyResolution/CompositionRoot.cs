dcd5e6713f68f9e835a66faddfefa1ed3be18e1b
David Higgins <d_higgins@imap.cc>
2018-07-04 19:02:41 +0100

    override for BuildServiceProvider to inject user's IoC FW

--- src/Noobot.Core/DependencyResolution/CompositionRoot.cs
+++ src/Noobot.Core/DependencyResolution/CompositionRoot.cs
@@ -15,18 +15,19 @@ namespace Noobot.Core.DependencyResolution
 {
     public static class CompositionRoot
     {
-        public static void Compose(IServiceCollection serviceCollection)
+        // So long as .BuildServiceProvider isn't used by us, this can count as abstract IoC
+        public static void Compose(IServiceCollection serviceCollection, Func<IServiceCollection, IServiceProvider> buildCustomServiceProvider = null)
         {
             // Container used to get config etc
-            ComposeMiddleware(serviceCollection);
+            ComposeMiddleware(serviceCollection, buildCustomServiceProvider ?? ServiceCollectionContainerBuilderExtensions.BuildServiceProvider);
             
             ComposePlugins(serviceCollection);
 
             ComposeNoobotCore(serviceCollection);
         }
-        static void ComposeMiddleware(IServiceCollection registry)
+        static void ComposeMiddleware(IServiceCollection registry, Func<IServiceCollection, IServiceProvider> buildCustomServiceProvider)
         {
-            var configProvider = registry.BuildServiceProvider();
+            var configProvider = buildCustomServiceProvider(registry);
             var configReader = configProvider.GetRequiredService<IConfigReader>();
 
             if (configReader.AboutEnabled)
